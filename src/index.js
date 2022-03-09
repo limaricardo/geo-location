4207f3e2141746df55ce8240c262e9b183b8e1b6
Ricardo <limaricardop@gmail.com>
2022-03-09 19:25:42 -0300

    Conditions to determine Seasons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
 
     state = { lat : null, errorMessage : '' };
 
     componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(position => this.setState({ lat : position.coords.latitude })
-            ,
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat : position.coords.latitude }),
             err => this.setState({ errorMessage : err.message })
             );
     }
@@ -18,7 +19,7 @@ class App extends React.Component {
         }
         
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
         else {
